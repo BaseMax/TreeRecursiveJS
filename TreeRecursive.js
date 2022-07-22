e4b1f2495de4b88313b767c8d1a1feb509718e72
Max Base <MaxBaseCode@Gmail.Com>
2022-07-22 21:14:37 +0200

    Update TreeRecursive.js

--- TreeRecursive.js
+++ TreeRecursive.js
@@ -1,1 +1,12 @@
 // https://github.com/BaseMax/TreeRecursiveJS
+
+const testList = [ 
+  { id: 1, name: "base", parentId: 1 }, 
+  { id: 2, name: "erfan", parentId: null }, 
+  { id: 3, name: "max", parentId: 1 }, 
+  { id: 4, name: "erfan", parentId: 2 }, 
+  { id: 5, name: "mamad", parentId: 3 }, 
+  { id: 6, name: "mojtaba", parentId: 8 }, 
+  { id: 7, name: "erfan", parentId: 4 }, 
+  { id: 8, name: "erfan", parentId: null }, 
+];
