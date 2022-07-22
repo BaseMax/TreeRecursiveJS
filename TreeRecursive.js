d5abf375c46a2670002e8edb4d9fcb4723be39ee
Max Base <maxbasecode@gmail.com>
2022-07-22 21:26:53 +0200

    done

--- TreeRecursive.js
+++ TreeRecursive.js
@@ -1,23 +1,27 @@
 // https://github.com/BaseMax/TreeRecursiveJS
 
 const testList = [ 
-  { id: 1, name: "base", parentId: 1 },
-  { id: 2, name: "erfan", parentId: null },
-  { id: 3, name: "max", parentId: 1 },
-  { id: 4, name: "erfan", parentId: 2 },
-  { id: 5, name: "mamad", parentId: 3 },
-  { id: 7, name: "erfan", parentId: 4 },
-  { id: 8, name: "erfan", parentId: null },
-  { id: 6, name: "mojtaba", parentId: 8 },
+  { id: 1, name: "base", parentId: null }, 
+  { id: 2, name: "erfan", parentId: 1 }, 
+  { id: 3, name: "max", parentId: 1 }, 
+  { id: 4, name: "bagher", parentId: 2 }, 
+  { id: 5, name: "mamad", parentId: 3 }, 
+  { id: 6, name: "mojtaba", parentId: 8 }, 
+  { id: 7, name: "mari", parentId: 4 }, 
+  { id: 8, name: "sara", parentId: null }, 
 ];
 
-const TreeRecursive = (list) => {
-  const tree = {};
+const TreeRecursive = (list, parent_id = null) => {
+  const tree = list.filter(item => item.parentId === parent_id);
+  tree.forEach(item => {
+    item.children = TreeRecursive(list, item.id);
+  });
   return tree;
 };
 
 const res = TreeRecursive(testList);
 console.log(res);
+console.log(JSON.stringify(res, null, 4));
 /*
 const newList = [ 
   { 
